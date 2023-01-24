f1df3923e74512e9a3f8b80efda34f7d2eba6ad3
wassy310 <ryg.wsza@gmail.com>
2023-01-24 17:23:17 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'STT',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Speech to Text'),
     );
   }
 }
