c7f31eb414a45d91e926b49cbc2ea936371dbf19
wassy310 <ryg.wsza@gmail.com>
2023-01-24 18:02:59 +0900

    Removed banner for debugging

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Speech to Text'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
