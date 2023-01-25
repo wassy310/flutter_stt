27fe83e09f71c66bf1ab898a45d52466e211222b
wassy310 <ryg.wsza@gmail.com>
2023-01-25 14:08:58 +0900

    Changed title

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Speech to Text'),
+      home: const MyHomePage(title: 'Home'),
       debugShowCheckedModeBanner: false,
     );
   }
